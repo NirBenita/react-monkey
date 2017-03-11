46dc756c5f624cf277f9b0d503eab2cca46d0f86
Nir Benita <nirb@nirb-mbp13.home>
2017-03-11 15:27:09 +0200

    linetd 👌

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,35 @@ import styled from 'styled-components';
 
 export const postMessage = (state, msg) => {
   state.push(msg);
-}
+};
 
 // 📢 A stream of messages
-const Messages = ({messages}) => {
-
-  const MessageFeed = messages.map((msg, i) => {
-
-    return (
-      <ConvoRow fromMe={msg.fromMe}>
-        <Message fromMe={msg.fromMe} key={i}>
-          <span>{msg.value}</span>
-        </Message>
-      </ConvoRow>
-    );
-  });
+const Messages = ({ messages }) => {
+  const MessageFeed = messages.map((msg, i) => (
+    <ConvoRow fromMe={msg.fromMe}>
+      <Message fromMe={msg.fromMe} key={i}>
+        <span>{msg.value}</span>
+      </Message>
+    </ConvoRow>
+    ));
 
   return (
     <div>
       {MessageFeed}
     </div>
   );
-}
+};
 
 const ConvoRow = styled.div`
   display:flex;
-  justify-content: ${ ({fromMe}) => fromMe ? 'flex-end' : 'flex-start'};
-`
+  justify-content: ${({ fromMe }) => fromMe ? 'flex-end' : 'flex-start'};
+`;
 
 const ResponseBar = styled.div`
   display:flex;
   flex-direction: row;
   justify-content: center;
-`
+`;
 
 // A single message
 const Message = styled.div`
@@ -44,53 +40,53 @@ const Message = styled.div`
     margin: 12px 8px;
     padding: 8px;
     min-width:32px;
-    background:${ ({fromMe}) => fromMe ? 'grey' : 'blue'};
-    color: ${ ({fromMe}) => fromMe ? 'black' : 'white'};
-    border-radius:${ ({fromMe}) => fromMe ? '8px 8px 0 8px' : '8px 8px 8px 0'};
-`
+    background:${({ fromMe }) => fromMe ? 'grey' : 'blue'};
+    color: ${({ fromMe }) => fromMe ? 'black' : 'white'};
+    border-radius:${({ fromMe }) => fromMe ? '8px 8px 0 8px' : '8px 8px 8px 0'};
+`;
 
 // 🗣 A list of possible responses
 const Responses = (props) => {
-  const ResponseList = props.responses.map((response, i) => {
-    return (
-      <Message
-        data-next={response.next}
-        key={i}
-        fromMe
-        response={response}
-        onClick={props.onMessageSubmit}>
-        {response.value}
-      </Message>
-    );
-  })
+  const ResponseList = props.responses.map((response, i) => (
+    <Message
+      data-next={response.next}
+      key={i}
+      fromMe
+      response={response}
+      onClick={props.onMessageSubmit}>
+      {response.value}
+    </Message>
+    ));
   return (
     <ResponseBar>
       {ResponseList}
     </ResponseBar>
-  )
-}
+  );
+};
 
 // The actual app
 export class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     const welcomeMessage = props.conversation[0];
 
     this.state = {
       messages: [welcomeMessage],
-      responses: welcomeMessage.replies
-    }
+      responses: welcomeMessage.replies,
+    };
 
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(e) {
     const messages = this.state.messages;
-    let userMessage = {
+    const userMessage = {
       value: e.target.innerHTML,
-      fromMe: true
-    }
-    const nextMessage = this.props.conversation.find(element => element.id === e.target.dataset.next)
+      fromMe: true,
+    };
+    const nextMessage = this.props.conversation.find(
+      element => element.id === e.target.dataset.next
+    );
 
     postMessage(messages, userMessage);
     postMessage(messages, nextMessage);
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import './index.css';
 import mockConversation from './mockConversation';
 
 ReactDOM.render(
   <App conversation={mockConversation} />,
-  document.getElementById('root')
+  document.getElementById('root');
 );
