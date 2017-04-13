ee9cdd8d871f764a3f5521e36fb09bbe7b9f658a
Nir Benita <benitanir@gmail.com>
2017-04-13 21:59:18 +0300

    Scroll to bottom on msg add

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {findDOMNode} from 'react-dom';
 
-import {
-  Messages,
-  Responses,
-  AppLayout,
-  Header
-} from './components/chat';
+import {Messages, Responses, AppLayout, Header} from './components/chat';
 
 function postMessages(messages) {
   let messagesToAdd = [...arguments]
   return function update(state) {
-    let newMessagesfeed = state.messages.slice();
+    let newMessagesfeed = state
+      .messages
+      .slice();
     newMessagesfeed.push(...messagesToAdd);
-    return {
-      messages: newMessagesfeed
-    }
+    return {messages: newMessagesfeed}
   }
 }
 
@@ -23,9 +18,9 @@ export class App extends Component {
   constructor(props) {
     super(props);
     const welcomeMessage = props.conversation[0];
-    const toPin = props.conversation.find(
-      element => element.id === 'toPin'
-    );
+    const toPin = props
+      .conversation
+      .find(element => element.id === 'toPin');
 
     this.state = {
       messages: [welcomeMessage],
@@ -34,13 +29,25 @@ export class App extends Component {
       toPin: toPin
     };
 
-    this.handleReply = this.handleReply.bind(this);
-    this.componentDidUpdate = this.componentDidUpdate.bind(this);
+    this.handleReply = this
+      .handleReply
+      .bind(this);
+    this.componentDidUpdate = this
+      .componentDidUpdate
+      .bind(this);
+  }
+
+  scrollToBottom = () => {
+    const node = findDOMNode(this.messagesEnd);
+    node.scrollIntoView({behavior: "smooth"});
+  }
+
+  componentDidMount() {
+    this.scrollToBottom();
   }
 
   componentDidUpdate() {
-    // var node = findDOMNode(this);
-    // node.scrollTop = node.scrollHeight;
+    this.scrollToBottom();
   }
 
   handleReply(userMessage) {
@@ -48,23 +55,24 @@ export class App extends Component {
     userMessage.fromMe = true;
 
     if (this.state.firstTime) {
-      this.setState({ firstTime: false })
+      this.setState({firstTime: false})
     };
-    
-    const nextMessage = this.props.conversation.find(
-      element => element.id === userMessage.next
-    );
 
+    const nextMessage = this
+      .props
+      .conversation
+      .find(element => element.id === userMessage.next);
 
     let responses = nextMessage.replies;
-    
+
     this.setState(postMessages(userMessage, nextMessage))
-    this.setState({ responses })
-    
-    if(nextMessage.chain){
-      const msgToChain = this.props.conversation.find(
-        element => element.id === nextMessage.chain
-      );
+    this.setState({responses})
+
+    if (nextMessage.chain) {
+      const msgToChain = this
+        .props
+        .conversation
+        .find(element => element.id === nextMessage.chain);
       this.setState(postMessages(msgToChain))
     }
   }
@@ -72,34 +80,39 @@ export class App extends Component {
   render() {
     return (
       <AppLayout className="App">
-          <Header>
-            <span> - TODAY - </span>
-          </Header>
-          <Messages messages={this.state.messages} />
-          <Responses
-            messages={this.state.responses}
-            onMessageSubmit={this.handleReply}
-            firstTime={this.state.firstTime} 
-          />
+        <Header>
+          <span>
+            - TODAY -
+          </span>
+        </Header>
+        <Messages messages={this.state.messages}/>
+        <Responses
+          messages={this.state.responses}
+          onMessageSubmit={this.handleReply}
+          firstTime={this.state.firstTime}/>
+        <div 
+          style={ {float:"left", clear: "both"} }
+          ref={(el) => { this.messagesEnd = el; }} />
       </AppLayout>
     );
   }
 }
-/* 
+/*
 TODO
   [x] Make sure replies render
   [x] Load full conversation
   [x] Add a click hint
   [x] Add left bar
-  [ ] Match Sketch styles
+  [x] Match Sketch styles
   [x] Refactor handleReply to pass the actual message
   [x] Allow the bot to chain messages
   [ ] Typing animation
   [x] Add markdown support
   [ ] Add Typescript support
   [ ] Add Image and Link message components
-  [ ] Scroll to bottom on msg add
+  [x] Scroll to bottom on msg add
   [x] bot avatar
   [ ] contact me shortcut
   [ ] Add current time
+  [ ] Rewrite and test
 */
